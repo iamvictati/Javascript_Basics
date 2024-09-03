0ff82cfe034f8ad86db93d43f80c883ae244cf86
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-09-03 17:29:31 -0400

    Modified index.html and script.js

--- DOM_Manipulation_and_Events/script.js
+++ DOM_Manipulation_and_Events/script.js
@@ -28,5 +28,47 @@ const div2p = document.createElement("p");
 div2p.textContent = "ME TOO!";
 div2.appendChild(div2p);
 container.appendChild(div2); 
+// CREATE A VARIABLE FOR EACH ELEMENT
 
-// CREATE A VARIABLE FOR EACH ELEMENT 
+
+//METHOD 2 & 3
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+}
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", alertFunction); 
+
+/* 
+METHOD 2
+btn.onclick = () => alert("Hello World!");  
+GOOD BUT WITH THIS WE CAN ONLY HAVE ONE ONCLICK PROPERTY  
+*/
+/* 
+METHOD 3
+btn.addEventListener("click", () => {
+    alert("Hello World!"); 
+});  
+
+Now, we maintain separation of concerns, and we also allow multiple event listeners if the need arises. Method 3 is much more flexible and powerful, though it is a bit more complex to set up.
+*/
+
+
+//This gives you insane info:
+btn.addEventListener("click", function (e) {
+    console.log(e);
+});
+
+//This tells you target:
+btn.addEventListener("click", function (e) {
+    console.log(e.target);
+});
+
+//This changes the button to blue once clicked: 
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+});
+
+
+
+
