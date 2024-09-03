1c016cf59a0ec3e583b0ae5a638b2df77ea9b745
iamvictati <142629886+iamvictati@users.noreply.github.com>
2024-09-03 16:46:40 -0400

    Modified script.js

--- DOM_Manipulation_and_Events/script.js
+++ DOM_Manipulation_and_Events/script.js
@@ -19,20 +19,14 @@ container.appendChild(h3);
 
 const div2 = document.createElement("div"); 
 div2.classList.add("div2"); 
+// div2.textContent = "WTF"; 
 div2.setAttribute("style", "background-color: pink; border-style: solid; border-color: black;"); 
-
+const div2h1 = document.createElement("h1"); 
+div2h1.textContent = "I'm in a div";
+div2.appendChild(div2h1);
+const div2p = document.createElement("p"); 
+div2p.textContent = "ME TOO!";
+div2.appendChild(div2p);
 container.appendChild(div2); 
 
-
-
- 
-
-
-
-/*
-a <div> with a black border and pink background color with the following elements inside of it:
-another <h1> that says “I’m in a div”
-a <p> that says “ME TOO!”
-Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
-
-*/
+// CREATE A VARIABLE FOR EACH ELEMENT 
